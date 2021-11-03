eebbc9d836f58de2e877daa16b84db15cf7901e5
Antoine Pietri <antoine.pietri1@gmail.com>
2021-11-03 04:11:11 +0100

    Update slides, add most of the script

--- codesamples/graph-dataset/weekend-work.sql
+++ codesamples/graph-dataset/weekend-work.sql
@@ -9,13 +9,13 @@ SELECT yearly_rev.year AS year,
 FROM
   (SELECT YEAR(date) AS year, COUNT(*) AS number
   FROM revision_date
-  WHERE YEAR(date) BETWEEN 1971 AND 2018
+  WHERE YEAR(date) BETWEEN 1971 AND 2020
   GROUP BY  YEAR(date) ) AS yearly_rev
 JOIN
   (SELECT YEAR(date) AS year, COUNT(*) AS number
   FROM revision_date
   WHERE DAY_OF_WEEK(date) >= 6
-      AND YEAR(date) BETWEEN 1971 AND 2018
+      AND YEAR(date) BETWEEN 1971 AND 2020
   GROUP BY  YEAR(date) ) AS yearly_weekend_rev
   ON yearly_rev.year = yearly_weekend_rev.year
 ORDER BY  year DESC;
